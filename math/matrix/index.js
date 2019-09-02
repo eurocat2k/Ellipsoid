7d2941b22f9112434eab5213b6c411ae60a51bf0
eurocat2k <gabor.zelenak@me.com>
2019-09-02 14:14:47 +0000

    Add files via upload

---
+++ math/matrix/index.js
@@ -0,0 +1,12 @@
+'use strict';
+const Point3D = require('./point3d/').Point3D;
+const Point4D = require('./point4d/').Point4D;
+const Vector3D = require('./vector3d/').Vector3D;
+const Matrix3D = require('./matrix3d/').Matrix3D;
+const Matrix4D = require('./matrix4d/').Matrix4D;
+
+exports.Point3D = Point3D;
+exports.Point4D = Point4D;
+exports.Vector3D = Vector3D;
+exports.Matrix3D = Matrix3D;
+exports.Matrix4D = Matrix4D;
